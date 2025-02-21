c128c939113efac35fe93b7086a3d9894a862612
Gonçalo Heleno <goncalocheleno@atomp.eu>
2025-02-21 13:26:19 +0100

    feat: add root command prototype

---
+++ cmd/root.go
@@ -0,0 +1,63 @@
+/*
+ * hledger-price-tracker - a CLI tool to get market prices for commodities
+ * Copyright (C) 2024 Gonçalo Carvalheiro Heleno
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "hledger-price-tracker",
+	Short: "A CLI tool to get market prices for commodities",
+	Long: `hledger-price-tracker
+
+hledger-price-tracker is a CLI program to generate market price 
+records for hledger, using the Alpha Vantage API and written in Go.`,
+
+	// Print the help message by default on the root command / bare application.
+	Run: func(cmd *cobra.Command, args []string) {
+		if error(cmd.Help()) != nil {
+			os.Exit(1)
+		}
+	},
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.hledger-price-tracker.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}

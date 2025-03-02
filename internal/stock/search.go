c50ca2410abab8da969c4fcf8db68be5fde3396b
Gonçalo Heleno <goncalocheleno@atomp.eu>
2025-03-02 12:08:52 +0100

    feat: create Go struct to represent JSON response

--- internal/stock/search.go
+++ internal/stock/search.go
@@ -30,6 +30,20 @@ import (
 
 const apiFunctionSearch = "SYMBOL_SEARCH"
 
+type SearchJSON struct {
+	BestMatches []struct {
+		Symbol      string `json:"1. symbol"`
+		Name        string `json:"2. name"`
+		Type        string `json:"3. type"`
+		Region      string `json:"4. region"`
+		MarketOpen  string `json:"5. marketOpen"`
+		MarketClose string `json:"6. marketClose"`
+		Timezone    string `json:"7. timezone"`
+		Currency    string `json:"8. currency"`
+		MatchScore  string `json:"9. matchScore"`
+	} `json:"bestMatches"`
+}
+
 func Search(query string, format flags.OutputFormat) (string, error) {
 	// Verify function parameters and variables.
 	if internal.ApiKey == "" {

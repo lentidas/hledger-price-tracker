fd33f71507de2f5894aa12026b9e27d132f08652
Gonçalo Heleno <goncalocheleno@atomp.eu>
2025-04-06 10:59:34 +0200

    fix: correct the last refresh parsing format

--- internal/currency/rate/rate.go
+++ internal/currency/rate/rate.go
@@ -57,7 +57,7 @@ type TypedMetadata struct {
 }
 
 func (typed *TypedMetadata) TypeBody(raw RawMetadata) error {
-	lastRefreshed, err := time.Parse("2006-01-02 15:04:05", raw.LastRefreshed)
+	lastRefreshed, err := time.Parse("2006-01-02", raw.LastRefreshed)
 	if err != nil {
 		return fmt.Errorf("[currency.rate.(*TypedMetadata).TypeBody] error parsing last refreshed date: %w", err)
 	}
